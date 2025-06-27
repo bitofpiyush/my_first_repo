406d39597153df4b3f8c68989a7778ea7a0286c6
Piyushhhhhh-design <bhattpiyush1686@gmail.com>
2025-06-27 06:11:01 +0545

    initial commit

---
+++ int_flo.py
@@ -0,0 +1,46 @@
+#integer and float 
+num =3
+print(type(num)) #datatype of num
+
+#Arithmetic operator
+#Addition 5 + 2
+#Subtraction 5 - 2
+#Multiplication 5 * 2
+#Division 5 / 2
+#Floor Division 5 // 2
+#Exponent 2 ** 3
+#Modulud 5 % 2
+ 
+print(3 ** 3) #It is equivalent to 3^3
+print(5 % 2)
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+print(5 / 2)
+print(5 // 2)
+
+numb=5
+numb+=1
+print(numb)
+numb*=5
+print(numb)
+
+number=-4
+print(abs(number)) #absolute value of number
+
+print(round(4.75,1)) #round off the 1st element after decimal
+
+#Comparision operator
+#Equal         5 = 2
+#Not Equal     5 !=2
+#less than     5 < 2
+#greater than  5  >2 
+#less than or equal    5 <= 2
+#greater than   or equal 5 >= 2
+
+#Typecasting: converts one type of data to another
+age = '20' #initially string
+age = int(age) #converts string to integer
+print(type(age))
+print(age)
+
