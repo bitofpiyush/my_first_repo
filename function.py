5321332df37dc3c94c6f394c26a6bea5e2214a1e
Piyushhhhhh-design <bhattpiyush1686@gmail.com>
2025-07-02 21:12:43 +0545

    initial commit

---
+++ function.py
@@ -0,0 +1,16 @@
+#function
+#to make function we use def(defination) keyword
+
+def hello():
+    print('Hello! everyone')
+    
+hello()
+hello()
+hello()
+hello()
+hello()
+
+def hello_world():
+    return "Hello World"
+
+print(hello_world())
