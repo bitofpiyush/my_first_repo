a392bdb4f0250186924df76482f9f0b690cda4b0
Piyushhhhhh-design <bhattpiyush1686@gmail.com>
2025-07-04 06:21:17 +0545

    Add more lines to function.py

--- function.py
+++ function.py
@@ -14,3 +14,25 @@ def hello_world():
     return "Hello World"
 
 print(hello_world())
+print(hello_world().upper())
+
+#argument
+def hello_func(greeting,name='David'):
+    return '{},{}'.format(greeting,name)
+print(hello_func('Hi'))
+
+#args and Kwargs
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+    
+student_info('Maths','CompSci',name='David',age=20)
+ 
+def stu_info(*args,**kwargs):
+    print(args)
+    print(kwargs)
+subject=['Maths','Arts']
+info={'name':'Ram','age':20}
+    
+stu_info(*subject,**info)    
+     
